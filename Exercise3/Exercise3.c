946e95dde45a3b9c987faf861cfc5cd544c67fff
Muhammad Farid <mfhosni94@gmail.com>
2021-02-01 14:28:50 +0200

    Third problem
    modifying the pointer arithmetic

--- Exercise3/Exercise3.c
+++ Exercise3/Exercise3.c
@@ -8,14 +8,16 @@
 #include<stdio.h>
 #define SIZE 10
 
-void ReverseArray(int *ptr){
+void ReverseArray(int *ptr){ /* setting a pointer to array */
 	int i;
 	int temp;
 
+	/* A loop to start swapping array elements in reverse order */
 	for(i=0 ; i<SIZE/2 ;i++){
-		temp = ptr[i];
-		ptr[i] = ptr[SIZE-1 - i];
-		ptr[SIZE-1 - i] = temp;
+		temp = *ptr;
+		*ptr = *(ptr + (SIZE-1) - i*2);
+		*(ptr + (SIZE-1) - i*2) = temp;
+		ptr++;
 	}
 }
 
@@ -24,21 +26,28 @@ int main(void){
 	setvbuf(stderr, NULL ,_IONBF , 0);
 	int arr[SIZE];
 	int i;
+	int *ptr = arr;
 
-
+	/* Scanning user for array elements */
 	printf("Enter array elements: ");
 	for(i=0 ; i<SIZE ;i++){
-		scanf("%d",&arr[i]);
+		scanf("%d",ptr);
+		ptr++;
 	}
 
+	/* Displaying array before re-arranging */
 	for(i=0 ; i<SIZE ;i++){
 		printf("Array element before re-arranging: arr[%d] = %d\n",i,arr[i]);
 	}
 
+	/* Sending array address to reverse function*/
 	ReverseArray(arr);
 
+	ptr = arr;           /* Re-setting the pointer to the first element in array */
+	/* Displaying array after re-arranging in descending order */
 	for(i=0 ; i<SIZE ;i++){
-		printf("Array element after re-arranging: arr[%d] = %d\n",i,arr[i]);
+		printf("Array element after re-arranging: arr[%d] = %d\n",i,*ptr);
+		ptr++;
 	}
 
 	return 0;
